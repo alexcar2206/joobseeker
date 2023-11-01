c6779e907bf9aab5783c3c2a9d6ea6d92e09f9a3
alexcar2206 <classfiles2223@gmail.com>
2023-11-01 13:53:37 +0100

    Modificacion servidor

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php
 
 
@@ -204,5 +196,3 @@ if(isset($_POST['service'])){
 }
 
 ?>
-</body>
-</html>
